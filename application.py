6998a8bce727bf60d231ef000227a7796b9424d6
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-03-10 15:05:41 -0600

    First story, user enters 3 data

--- application.py
+++ application.py
@@ -1,3 +1,13 @@
-# Aqui escribe tu codigo
+import re
+import urllib2 
 
-print "Hello World"
+class Find(object):
+	def __init__(self, word, url1, url2):
+		self.word = word
+		self.url1 = url1
+		self.url2 = url2
+
+
+
+
+User = Find(raw_input("Enter a word to be searched: "), raw_input("Enter a valid URL: "), raw_input("Enter the second valid URL: "))
