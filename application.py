bd2850f7bbf47e398b83dd0819b7d34db1b06783
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-03-11 16:27:46 -0600

    Second story done, it has bugs in the try again fase

--- application.py
+++ application.py
@@ -1,13 +1,63 @@
 import re
-import urllib2 
+import urllib2
+import sys
 
 class Find(object):
-	def __init__(self, word, url1, url2):
-		self.word = word
-		self.url1 = url1
-		self.url2 = url2
+    def search(self):
+        """Asks the user for two urls and one word, searches for matches"""
+        while True:
+            try:
+                self.word = raw_input("Enter a word to be searched: ")
+                self.url1 = raw_input("Enter a valid URL (remember to add https://): ")
+                self.url2 = raw_input("Enter the second valid URL (remember to add https://): ")
+
+                self.word = self.word.lower()
+                self.url1 = self.url1.lower()
+                self.url2 = self.url2.lower()
+
+                web = urllib2.urlopen(self.url1)
+                web2 = urllib2.urlopen(self.url2)
+                read = web.read()
+                read2 = web2.read()
+
+                s1 = len(re.findall(self.word, read))
+                s2 = len(re.findall(self.word, read2))
+
+                if s1 > s2:
+                    print "The URL with more repetitions is: ", self.url1
+                    print "The number of times the word was found is: ", s1
+                elif s2 > s1:
+                    print "The URL with more repetitions is: ", self.url2
+                    print "The number of times the word was found is: ", s2
+                elif s2 == s1:
+                    print "Both URL's have the word the same time: ", s1, "-", s2
+                else:
+                    print "Wow, What is going on"
+
+                PROOF.tryagain()
+
+            except:
+                print "Error"
+
+
+    def tryagain(self):
+        try:
+            print "Do you want to search for another word? Y/N"
+            user = raw_input(">")
+            user = user.lower()
+            if user == "y":
+                PROOF.search()
+            elif user == "n":
+                print "Good-Bye"
+                sys.exit()
+            else:
+                print "Invalid option, enter only Y or N"
+        except: 
+            print "Mysterious Error"
 
 
 
+PROOF = Find()
+PROOF.search()
 
-User = Find(raw_input("Enter a word to be searched: "), raw_input("Enter a valid URL: "), raw_input("Enter the second valid URL: "))
+
