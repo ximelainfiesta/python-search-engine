253a97d2ad90ce72e0e8a1fddad2879cf23f0997
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-03-12 14:56:32 -0600

    Finished stories, trying to match searched with another formula

--- application.py
+++ application.py
@@ -1,42 +1,56 @@
+"""Search Engine"""
+
 import re
 import urllib2
 import sys
+#We import some modules
 
 class Find(object):
+    """Class that searched for words in urls"""
     def search(self):
         """Asks the user for two urls and one word, searches for matches"""
         while True:
-            self.word = raw_input("Enter a word to be searched: ")
-            self.url1 = raw_input("Enter a valid URL (remember to add https://): ")
-            self.url2 = raw_input("Enter the second valid URL (remember to add https://): ")
+            try:
+                word = raw_input("Enter a word to be searched: ")
+                url1 = raw_input("Enter a valid URL (remember to add https://): ")
+                url2 = raw_input("Enter the second valid URL (remember to add https://): ")
 
-            self.word = self.word.lower()
-            self.url1 = self.url1.lower()
-            self.url2 = self.url2.lower()
+                word = word.lower()
+                url1 = url1.lower()
+                url2 = url2.lower()
 
-            web = urllib2.urlopen(self.url1)
-            web2 = urllib2.urlopen(self.url2)
-            read = web.read()
-            read2 = web2.read()
+                web = urllib2.urlopen(url1)#opens url
+                web2 = urllib2.urlopen(url2)
+                read = web.read() #reads the file
+                read2 = web2.read()
 
-            s1 = len(re.findall(self.word, read))
-            s2 = len(re.findall(self.word, read2))
+                search1 = len(re.findall(word, read)) #searches the given word in the file
+                search2 = len(re.findall(word, read2))
 
-            if s1 > s2:
-                print "The URL with more repetitions is: ", self.url1
-                print "The number of times the word was found is: ", s1
-            elif s2 > s1:
-                print "The URL with more repetitions is: ", self.url2
-                print "The number of times the word was found is: ", s2
-            elif s2 == s1:
-                print "Both URL's have the word the same time: ", s1, "-", s2
-            else:
-                print "Wow, What is going on"
+                web.close() #close the file
+                web2.close()
 
-            PROOF.tryagain()
+                if search1 > search2:
+                    print "The URL with more repetitions is: ", url1
+                    print "The number of times the word was found is: ", search1
+                elif search2 > search1:
+                    print "The URL with more repetitions is: ", url2
+                    print "The number of times the word was found is: ", search2
+                elif search2 == search1:
+                    print "Both URL's have the word the same time: ", search1, "-", search2
+                else:
+                    print "Wow, What is going on?"
 
+                PROOF.tryagain() #initiates the try again method
+            except ValueError:
+                print "Enter a Valid URL with https:// included"
+            except NameError:
+                print "-Enter a Valid URL with https:// included"
+            except TypeError:
+                print "--Enter a Valid URL with https:// included"
 
     def tryagain(self):
+        """Asks the user for another search"""
         while True:
             print "Do you want to search for another word? Y/N"
             user = raw_input(">")
@@ -45,12 +59,9 @@ class Find(object):
                 PROOF.search()
             elif user == "n":
                 print "Good-Bye"
-                False
                 sys.exit()
             else:
                 print "Invalid option, enter only Y or N"
-
-
 
 PROOF = Find()
 PROOF.search()
